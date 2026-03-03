74172e74b221c2aa8e39feea287d960aa9fa088c
Marko Musica <markomusica@gmail.com>
2026-03-03 18:13:22 +0100

    e

--- hub-backend/src/main/java/com/hub/form/model/User.java
+++ hub-backend/src/main/java/com/hub/form/model/User.java
@@ -5,15 +5,12 @@ import lombok.Setter;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.time.Instant;
-import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.NaturalId;
@@ -23,7 +20,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Setter
 @Entity
 @Table(name = "`user`")
-public class User {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,9 +38,6 @@ public class User {
 
     @Column(name = "email")
     private String email;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = Form_.USER)
-    private Set<Form> forms;
 
     @CreationTimestamp
     @Column(name = "created_at")
